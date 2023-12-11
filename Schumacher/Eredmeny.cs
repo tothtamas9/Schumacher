98d27a9613ec332df29faa1d3896087f6156f2ef
tothtamas9 <71500475+tothtamas9@users.noreply.github.com>
2023-12-11 16:44:06 +0100

    Add project files.

---
+++ Schumacher/Eredmeny.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schumacher
+{
+    public class Eredmeny
+    {
+        DateTime date;
+        string grandprix;
+        int position;
+        int laps;
+        int points;
+        string team;
+        string status;
+
+        public Eredmeny(DateTime date, string grandprix, int position, int laps, int points, string team, string status)
+        {
+            this.date = date;
+            this.grandprix = grandprix;
+            this.position = position;
+            this.laps = laps;
+            this.points = points;
+            this.team = team;
+            this.status = status;
+        }
+
+        public DateTime Date { get => date; }
+        public string Grandprix { get => grandprix; }
+        public int Position { get => position; }
+        public int Laps { get => laps; }
+        public int Points { get => points; }
+        public string Team { get => team; }
+        public string Status { get => status; }
+    }
+}
---
+++ Schumacher/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Schumacher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Eredmeny> eredmenyek = new List<Eredmeny>();
+            string[] olvasottEredmenyek = File.ReadAllLines("Source\\schumacher.csv");
+            for (int i = 1; i < olvasottEredmenyek.Length; i++)
+            {
+                string[] tagok = olvasottEredmenyek[i].Split(';');
+                Eredmeny ujEredmeny = new Eredmeny(DateTime.Parse(tagok[0]), tagok[1], int.Parse(tagok[2]), int.Parse(tagok[3]), int.Parse(tagok[4]), tagok[5], tagok[6]);
+                eredmenyek.Add(ujEredmeny);
+            }
+
+            //3.feladat:
+            Console.WriteLine($"3. feladat: {eredmenyek.Count()}");
+
+            //4.feladat:
+            Console.WriteLine("Magyar Nagydíj helyezései");
+            foreach (var i in eredmenyek)
+            {
+                if (i.Grandprix == "Hungarian Grand Prix" && i.Position > 0)
+                {
+                    Console.WriteLine($"\t{i.Date.ToShortDateString()} : {i.Position}. hely");
+                }
+            }
+
+            //5.feladat:
+            Console.WriteLine("5. feladat: Hibastatisztika");
+
+        }
+    }
+}
